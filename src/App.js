9280330822c0a81c9a547faf4f5883e8f0f09df8
Kate Eldridge <keldridge@somethingdigital.com>
2018-10-20 14:43:29 -0400

    Set up react router with dummy pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    return <Router>
       <div className="App">
-        
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={Profile} />
+        <ul>
+          <li><Link to={`/profile`}>Profile Page</Link></li>
+          <li><Link to={`/login`}>Login Page</Link></li>
+        </ul>
       </div>
-    );
+      </Router>
   }
 }
 
