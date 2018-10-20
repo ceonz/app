b7046d0c90d85c4c2029ac2fb0bdd0e8f1f1e5fe
Kyle Leighton <kyle.leighton1@gmail.com>
2018-10-20 14:56:43 -0700

    Create basic homepage component
    And remove unused links

---
+++ src/components/Homepage/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Avatar } from 'evergreen-ui';
+
+class Homepage extends Component {
+
+  render() {
+    return (
+      <div className="homepage">
+        <Avatar
+          src="https://pbs.twimg.com/profile_images/915954899925467136/MQFzXfTQ_400x400.jpg"
+          size={150}
+          className="home-avatar"
+        />
+        <button type="submit">Create A Fund</button>
+        <button type="submit">See List of Funds</button>
+        <div className="credits">
+          <h3>Created by:</h3>
+          <ul>
+            <li>Kate Eldridge</li>
+            <li>Gil Greenberg</li>
+            <li>Kyle Leighton</li>
+            <li>Ethan Yehuda</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
