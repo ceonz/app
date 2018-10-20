7e80abd749f53688f036c5625d6a7b6062afff8d
Kyle Leighton <kyle.leighton1@gmail.com>
2018-10-20 15:21:41 -0700

    Add all non-interactive content to fund profile page

--- src/components/FundProfile/index.js
+++ src/components/FundProfile/index.js
@@ -6,9 +6,42 @@ class FundProfile extends Component {
 
   render() {
     return (
-      
+      <div>
+        <h2>Fund Name</h2>
+        {this.props.listItems.map((listItem) => (
+          <div key={listItem.reference}>
+            <label htmlFor={listItem.reference}>{`${listItem.label}: `}</label>
+            <p></p>
+          </div>
+        ))}
+      </div>
     );
   }
 }
+
+FundProfile.defaultProps = {
+  listItems: [
+    {
+      label: 'Fund Name',
+      reference: 'fundName',
+    },
+    {
+      label: 'Fund ID',
+      reference: 'fundId',
+    },
+    {
+      label: 'Fund Owner',
+      reference: 'fundOwner',
+    },
+    {
+      label: 'Fund Description',
+      reference: 'fundDescription',
+    },
+    {
+      label: 'Fund Total',
+      reference: 'fundTotal',
+    },
+  ]
+};
 
 export default FundProfile;
