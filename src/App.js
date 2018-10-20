ff19a88ee097b69865bf9cac87b472d4d02b25d1
Kyle Leighton <kyle.leighton1@gmail.com>
2018-10-20 15:14:04 -0700

    Create FundProfile component scaffolding and include possible banners

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CreditCheck from './components/CreditCheck';
+import FundProfile from './components/FundProfile';
 import FundRegistration from './components/FundRegistration';
 import Homepage from './components/Homepage';
 import React, { Component } from 'react';
@@ -13,10 +14,12 @@ class App extends Component {
         <Route exact path="/home" component={Homepage} />
         <Route exact path="/credit-check" component={CreditCheck} />
         <Route exact path="/register-fund" component={FundRegistration} />
+        <Route exact path="/fund-profile" component={FundProfile} />
         <ul>
           <li><Link to={`/home`}>Home</Link></li>
           <li><Link to={`/credit-check`}>Credit Check</Link></li>
           <li><Link to={`/register-fund`}>Register Fund</Link></li>
+          <li><Link to={`/fund-profile`}>Register Fund</Link></li>
         </ul>
       </div>
       </Router>
---
+++ src/components/FundProfile/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Failure from '../Banners/Failure';
+import Success from '../Banners/Success';
+
+class FundProfile extends Component {
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default FundProfile;
