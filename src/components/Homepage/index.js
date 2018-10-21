040dedb8044a7c64b251874f445b011f6bcd9917
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-10-21 05:06:26 -0700

    Cleanup buttons on homepage and add working history push

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import { Avatar } from 'evergreen-ui';
+import { Avatar, Button } from 'evergreen-ui';
 
 class Homepage extends Component {
+  handleCreateFundClick = () => {
+    this.props.history.replace(`/credit-check`);
+  }
+
+  handleListFundsClick = () => {
+    this.props.history.replace(`/funds/`);
+  }
 
   render() {
-    return (
-      <div className="homepage">
-        <Avatar
-          src="https://pbs.twimg.com/profile_images/915954899925467136/MQFzXfTQ_400x400.jpg"
-          size={150}
-          className="home-avatar"
-        />
+    return <div className="homepage">
+        <Avatar src="https://pbs.twimg.com/profile_images/915954899925467136/MQFzXfTQ_400x400.jpg" size={150} className="home-avatar" />
         <div className="homepage-buttons">
-          <button type="submit"><a href="/credit-check">Create A Fund</a></button>
-          <button type="submit"><a href="/fund-profile">See List of Funds</a></button>
+          <Button
+            justifyContent="center"
+            height={48}
+            marginBottom={`15px`}
+            iconBefore="bank-account"
+            onClick={this.handleCreateFundClick}
+          >
+            Start Your Community Fund
+          </Button>
+          <Button
+            justifyContent="center"
+            height={48}
+            marginBottom={`15px`}
+            iconBefore="th-list"
+            onClick={this.handleListFundsClick}
+          >
+            See List of Community Funds
+          </Button>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
